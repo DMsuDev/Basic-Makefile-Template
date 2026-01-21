5c6998549d4df5321832a379166d2c5bb884ab60
Dayron <dayrondotres@gmail.com>
2026-01-21 05:09:07 +0100

    Add donut demo to basic examples

---
+++ examples/donut-basic/src/main.cpp
@@ -0,0 +1,23 @@
+// Tiny main that delegates to the donut module (keeps makefile demo visible)
+#include <iostream>
+#include <algorithm>
+#include "donut.h"
+
+int main(int argc, char** argv)
+{
+	donut::Config cfg;
+	if (argc >= 3)
+	{
+		cfg.width = std::max(20, std::atoi(argv[1]));
+		cfg.height = std::max(10, std::atoi(argv[2]));
+	}
+	if (argc >= 5)
+	{
+		cfg.frames = std::atoi(argv[3]);
+		cfg.fps = std::max(1, std::atoi(argv[4]));
+	}
+
+	donut::run(cfg);
+	
+	return 0;
+}
